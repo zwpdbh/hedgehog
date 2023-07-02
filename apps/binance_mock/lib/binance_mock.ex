0911c16be6f4df52d454b3866c3979f5aea61554
zwpdbh <hyperion_z@outlook.com>
2023-07-02 15:53:49 +0800

    > 4.5

--- apps/binance_mock/lib/binance_mock.ex
+++ apps/binance_mock/lib/binance_mock.ex
@@ -1,18 +1,78 @@
 defmodule BinanceMock do
   @moduledoc """
-  Documentation for `BinanceMock`.
+  This module will mock Binance module to
+  1. generate and broadcast fake trade event
+  2. cover the REST functions
+  3. store buy and sell orders
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  alias Decimal, as: D
+  alias Streamer.Binance.TradeEvent
 
-      iex> BinanceMock.hello()
-      :world
+  require Logger
 
-  """
-  def hello do
-    :world
+  defmodule State do
+    defstruct order_books: %{}, subscriptions: [], fake_order_id: 1
+  end
+
+  defmodule OrderBook do
+    defstruct buy_side: [], sell_side: [], historical: []
+  end
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(_args) do
+    {:ok, %State{}}
+  end
+
+  def handle_cast(
+        {:add_order, %Binance.Order{symbol: symbol} = order},
+        %State{order_books: order_books, subscriptions: subscriptions} = state
+      ) do
+    new_subscriptions = subscribe_to_topic(symbol, subscriptions)
+    updated_order_books = add_order(order, order_books)
+
+    {
+      :noreply,
+      %{
+        state
+        | order_books: updated_order_books,
+          subscriptions: new_subscriptions
+      }
+    }
+  end
+
+  def get_exchange_info do
+    Binance.get_exchange_info()
+  end
+
+  def order_limit_buy(symbol, quantity, price, "GTC") do
+    order_limit(symbol, quantity, price, "BUY")
+  end
+
+  def order_limit_sell(symbol, quantity, price, "GTC") do
+    order_limit(symbol, quantity, price, "SELL")
+  end
+
+  defp order_limit(symbol, quantity, price, side) do
+    %Binance.Order{} =
+      fake_order =
+      generate_fake_order(
+        symbol,
+        quantity,
+        price,
+        side
+      )
+
+    GenServer.cast(
+      __MODULE__,
+      {:add_order, fake_order}
+    )
+
+    {:ok, convert_order_to_order_response(fake_order)}
   end
 end
